cbdecb330376b429a52602c1e90514ba95af2b9c
Durso <dursozd@gmail.com>
2021-04-21 15:30:45 -0400

    style

--- app.js
+++ app.js
@@ -8,18 +8,26 @@ let time = date.getTime();
 let h = date.getHours();
 let m = date.getMinutes();
 let s = date.getSeconds();
-let mili = date.getMilliseconds();
 let A = " AM"
-if(h> 11) {A = " PM";}
-if(h == 24){let h = "MIDNIGHT!!"}
+if(h > 11) {A = " PM";}
+if(h < 10) {h = "0" + h;}
+if(h >= 13) {h =  "0" + (h - 12)}
+if(h == 24) {A = " AM"; h = 12;}
+if(m < 10) {m = "0" + m;}
+if(s < 10) { s = "0" + s;}
 
-let clock = document.querySelector("#clock");
+const mv = (m /60) * 360;
+document.querySelector(".min").style.transform = "rotate("+ mv+"deg)";
 
+const hv = (h /12) * 360;
+document.querySelector(".hour").style.transform = "rotate("+ hv+"deg)";
 
+const sv = (s /60) * 360;
+document.querySelector(".sec").style.transform = "rotate("+ sv+"deg)";
 
+document.querySelector(".hour").innerHTML = h + ":" + "<br>" + m + "<br>" + A;
 
-
-let renderTime =  h + ":" + m + ":" + s + ":" + A;
+let renderTime =  h + ":" + m + ":" + s + ":"  + A;
 let render = document.querySelector("#render");
 render.innerHTML = renderTime;
 
